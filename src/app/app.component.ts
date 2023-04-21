778c3d3cda10fbaef25e6b7fe8c0867b7a01014f
Joao Mota <joaoefmota@gmail.com>
2023-04-21 15:03:00 +0200

    tour of heroes tuto done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'myApp';
+  title = 'Tour of Heroes';
+  obj = {
+    name: "Object",
+    print: function () {
+      console.log(this)
+    },
+  };
+
+  constructor() {
+    this.obj.print();
+  }
 }
+
+
